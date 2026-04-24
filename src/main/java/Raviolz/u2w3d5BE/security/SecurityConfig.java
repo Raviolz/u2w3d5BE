74793981d42ddaae1bc902d483d9441879c8194c
Raviolz <gi.ravioli@gmail.com>
2026-04-24 12:36:54 +0200

    Configuro Impostazioni Spring Security

---
+++ src/main/java/Raviolz/u2w3d5BE/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package Raviolz.u2w3d5BE.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+
+@EnableMethodSecurity
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+    @Bean
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+
+        httpSecurity.authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll());
+
+        httpSecurity.sessionManagement(sessions -> sessions.sessionCreationPolicy(STATELESS));
+
+        httpSecurity.formLogin(formLogin -> formLogin.disable());
+
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+}
