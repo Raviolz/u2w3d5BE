01ac619e5f9ed0d9dd6ffd2b5bd2b4e519bb19b4
Raviolz <gi.ravioli@gmail.com>
2026-04-24 10:39:06 +0200

    Initial commit

---
+++ src/main/java/Raviolz/u2w3d5BE/U2w3d5BeApplication.java
@@ -0,0 +1,13 @@
+package Raviolz.u2w3d5BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2w3d5BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2w3d5BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Raviolz/u2w3d5BE/U2w3d5BeApplicationTests.java
@@ -0,0 +1,13 @@
+package Raviolz.u2w3d5BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2w3d5BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
