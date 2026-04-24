85fb56003b24f13fa303c9642a521c1c155eb948
Raviolz <gi.ravioli@gmail.com>
2026-04-24 13:58:37 +0200

    Token Tools Metodi

--- src/main/java/Raviolz/u2w3d5BE/security/SecurityConfig.java
+++ src/main/java/Raviolz/u2w3d5BE/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -29,5 +31,10 @@ public class SecurityConfig {
         httpSecurity.csrf(csrf -> csrf.disable());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder getBCrypt() {
+        return new BCryptPasswordEncoder(12);
     }
 }
---
+++ src/main/java/Raviolz/u2w3d5BE/security/TokenTools.java
@@ -0,0 +1,42 @@
+package Raviolz.u2w3d5BE.security;
+
+import Raviolz.u2w3d5BE.entities.Utente;
+import Raviolz.u2w3d5BE.exception.UnauthorizedException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+
+@Component
+public class TokenTools {
+    private final String secret;
+
+    public TokenTools(@Value("${jwt_secret}") String secret) {
+        this.secret = secret;
+    }
+
+    public String generateToken(Utente utente) {
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                .subject(String.valueOf(utente.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    ;
+
+
+    public void verifyToken(String token) {
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+        } catch (Exception ex) {
+            throw new UnauthorizedException("Problemi con il token, effettua di nuovo il login!");
+
+
+        }
+    }
+}
